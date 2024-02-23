03d477a01c53f18324617d24461f2eb668db8699
Utpal Paul <utpalseu@gmail.com>
2024-02-23 23:04:54 +0600

    Survivorship Module Done

--- src/webScraping/constants/__init__.py
+++ src/webScraping/constants/__init__.py
@@ -1,2 +1,17 @@
-skip_contents = ['more in this section', 'related resources', 'more information', 'learn more with free videos', 'follow #asco23 news anywhere', 'May 25']
+skip_contents = ['more in this section', 
+                 'related resources', 'more information', 
+                 'learn more with free videos', 
+                 'follow #asco23 news anywhere', 
+                 'May 25', 
+                 'Tags:',
+                 'Cancer.Net Podcasts',
+                 'RSS Feed',
+                 'Policies and Guidelines:',
+                 'Contact Us:',
+                 'Category:',
+                 'Archives by Month:',
+                 'Other survivorship resources on Cancer.Net'
+
+
+                 ]
 partially_skip_contents = ['asco annual meeting']
